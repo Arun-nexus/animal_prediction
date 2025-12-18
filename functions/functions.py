6d5709431052090503b9b092e6e64f7ef29d6b13
Arun-nexus <arun999x0@gmail.com>
2025-12-18 21:59:45 +0530

    upgradation in model

--- functions/functions.py
+++ functions/functions.py
@@ -2,6 +2,7 @@ from database.animal_information import get_animal_details, class_names
 from model.cnn_model import model, device
 from torchvision import transforms
 from PIL import Image
+import io
 import torch.nn.functional as F
 import torch
 
@@ -14,35 +15,31 @@ transform = transforms.Compose([
     )
 ])
 
-def image_name(image_path):
-    try:
-        img = Image.open(image_path).convert("RGB")
-        img = transform(img)
-        img = img.unsqueeze(0).to(device)
+async def image_name(image):
 
+        # 1. UploadFile se raw bytes read karein
+        img_bytes = await image.read()
+
+        # 2. Bytes ko PIL Image mein convert karein
+        img = Image.open(io.BytesIO(img_bytes)).convert('RGB')
+
+        # 3. Transform apply karein (jo Tensor banayega)
+        # Maan lete hain aapka transform variable ka naam 'transform' hai
+        img_tensor = transform(img).unsqueeze(0)  # Batch dimension add karein
+
+        # 4. Model Prediction
         model.eval()
         with torch.no_grad():
-            logits = model(img)
-            probs = F.softmax(logits, dim=1)
-
-
+            logits = model(img_tensor)  # Ab yahan Tensor jayega, UploadFile nahi
+            probs = torch.nn.functional.softmax(logits, dim=1)
             top3_prob, top3_idx = torch.topk(probs, 3, dim=1)
 
-            top3_prob = top3_prob[0].tolist()
-            top3_idx = top3_idx[0].tolist()
-            top3_prob = [round(p, 2) for p in top3_prob]
+        return top3_prob[0].tolist(), top3_idx[0].tolist()
 
-            return top3_prob,top3_idx
-
-    except Exception as e:
-        return {"error": str(e)}
-
-image = r"C:\Users\Arun\Downloads\images.webp"
-prediction = image_name(image)
-prob,out = prediction
-
-def info(out,prob):
+def info(out, prob):
     info_list = {}
-    for n in range(len(out)):
-         info_list[class_names[out[n]]]=prob[n]
-    return get_animal_details(out[0]),info_list
+    for i in range(len(out)):
+        info_list[class_names[out[i]]] = prob[i]
+
+    main_animal_info = get_animal_details(out[0])
+    return main_animal_info, info_list
